a44a5a580b8df53ec2472a0eea541aeba9a2bc3a
Ivan <work.gorobec@gmail.com>
2021-10-07 00:27:03 +0500

    добавит маршрутизацию в приложение

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -1,4 +1,5 @@
 import OfferCard from '../offer-card/offer-card';
+import Logo from '../logo/logo';
 
 type MainScreenProps = {
   offersCount: number;
@@ -31,11 +32,7 @@ function MainScreen({offersCount}:MainScreenProps): JSX.Element {
         <header className="header">
           <div className="container">
             <div className="header__wrapper">
-              <div className="header__left">
-                <a className="header__logo-link header__logo-link--active">
-                  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-                </a>
-              </div>
+              <Logo />
               <nav className="header__nav">
                 <ul className="header__nav-list">
                   <li className="header__nav-item user">
